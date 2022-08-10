3d834e7448925e25bd2cf60b4b31d3251ccaa894
ferminpatdujr <fpatdu@77global.biz>
2022-08-10 18:31:15 +0800

    commit changes

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Final Examination',
+    day: 'August 25th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting with Clients',
+    day: 'September 6th at 4:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Collection of Credits',
+    day: 'October 18th at 12:30pm',
+    reminder: false,
+  },
+];
